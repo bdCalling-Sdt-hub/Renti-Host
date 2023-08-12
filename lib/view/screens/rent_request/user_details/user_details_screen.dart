3422723880b9759d4a3bbdec782c33a2caf4d152
jubayed004 <mohammadjubayed.islam97@gmail.com>
2023-08-12 06:47:57 -0700

    feat(message): demo_screen_design

--- lib/view/screens/rent_request/user_details/user_details_screen.dart
+++ lib/view/screens/rent_request/user_details/user_details_screen.dart
@@ -10,6 +10,8 @@ class UserDetailsScreen extends StatefulWidget {
 class _UserDetailsScreenState extends State<UserDetailsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+
+    );
   }
 }
--- lib/view/screens/rent_request/user_request/user_request_screen.dart
+++ lib/view/screens/rent_request/user_request/user_request_screen.dart
@@ -10,6 +10,57 @@ class UserRequestScreen extends StatefulWidget {
 class _UserRequestScreenState extends State<UserRequestScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return SafeArea(
+        child: Scaffold(
+          backgroundColor: Color(0xffffffff),
+      appBar: AppBar(
+        elevation: 0,
+          backgroundColor: Colors.white,
+          title: Text(
+            "Users Request",
+            style: TextStyle(color: Color(0xff2E2C2C)),
+          )),
+       body: Column(
+         // crossAxisAlignment: CrossAxisAlignment.start,
+         children: [
+           Padding(
+             padding: EdgeInsets.all(33.0),
+             child: SizedBox(
+               width: 350,
+               height: 200,
+               child: Container(
+                 width: 350,
+                 height: 200,
+                 decoration: BoxDecoration(
+                     borderRadius: BorderRadius.circular(8.0),
+                      color: Colors.white,
+                     boxShadow:  [
+                       BoxShadow(
+                           color: Colors.black.withOpacity(0.1),
+                           blurRadius: 10,
+                          offset: Offset(1,0),
+                         spreadRadius: 0
+
+                       )
+                     ]
+                 ),
+                 child: Column(
+                   children: [
+                     Padding(
+                       padding: const EdgeInsetsDirectional.only(start: 16,top: 16),
+                       child: Row(
+                         children: [
+                           Image(image: AssetImage("assets/images/Avater.png"),width: 50,height: 50,),
+                           ],
+                       ),
+                     )
+                   ],
+                 ),
+               ),
+             ),
+           )
+         ],
+       ),
+    ));
   }
 }
