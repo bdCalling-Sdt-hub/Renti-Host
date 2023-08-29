2977044ee77a8f571d5eac5b16a32c0673d8e9f1
humayunkabir11 <pythonloverkabir11@gmail.com>
2023-08-29 11:01:58 +0600

    fix(message): incomplete

--- lib/view/screens/message/messages/inner_widgets/message_list.dart
+++ lib/view/screens/message/messages/inner_widgets/message_list.dart
@@ -129,6 +129,8 @@ class MessageList extends StatelessWidget {
                 );
                 },
 
+
+
                 backgroundColor: const Color(0xFFD7263D),
                 icon : CupertinoIcons.delete,
               ),
